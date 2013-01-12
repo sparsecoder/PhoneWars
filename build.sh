416b971f1d550447c788d98a83a1e36bb54ba8dc
andrew <stevens>
2013-01-11 16:17:55 -0800

    jenkins... grrr

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$USER" != "jenkins" ]
+then
+  exit 1
+fi
+
+tidy -modify -indent -f index_htmlErrors.txt index.html;
+if [["$?" -gt "1"]]
+then
+  exit 2
+fi
+
+set -e
+
+
+stat=`git status --porcelain`
+stlen=${#stat}
+if [["$stlen" -gt "0"]]
+then
+  git add index_htmlErrors.txt index.html
+  git commit -m "Jenkins"
+  git checkout master
+  git merge HEAD@{1}
+  git push
+  git push origin master:gh-pages
+fi
